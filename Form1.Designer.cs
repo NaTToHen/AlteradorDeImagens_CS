5511a4a0e787e09fda996d72fba2b0780056c76c
Henrique Bonatto <89865580+NaTToHen@users.noreply.github.com>
2024-04-02 21:46:13 -0300

    teste2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -26,25 +26,31 @@
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
-        {
+        private void InitializeComponent() {
             titulo = new Label();
             btnImagem1 = new Button();
             imgCarregada = new PictureBox();
             imgFinal = new PictureBox();
             btnCinza = new Button();
+            btnBrilho = new Button();
+            valorBrilho = new TextBox();
+            btnNegativo = new Button();
+            btnContraste = new Button();
+            imgCarregada2 = new PictureBox();
+            btnImagem2 = new Button();
             ((System.ComponentModel.ISupportInitialize)imgCarregada).BeginInit();
             ((System.ComponentModel.ISupportInitialize)imgFinal).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)imgCarregada2).BeginInit();
             SuspendLayout();
             // 
             // titulo
             // 
             titulo.Anchor = AnchorStyles.Top;
-            titulo.Font = new Font("JetBrains Mono NL", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            titulo.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             titulo.ForeColor = Color.White;
-            titulo.Location = new Point(425, 31);
+            titulo.Location = new Point(701, 32);
             titulo.Name = "titulo";
-            titulo.Size = new Size(232, 25);
+            titulo.Size = new Size(281, 25);
             titulo.TabIndex = 0;
             titulo.Text = "Alterador de imagens";
             titulo.TextAlign = ContentAlignment.TopCenter;
@@ -52,10 +58,10 @@
             // btnImagem1
             // 
             btnImagem1.Anchor = AnchorStyles.None;
-            btnImagem1.Font = new Font("JetBrains Mono", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnImagem1.Location = new Point(77, 394);
+            btnImagem1.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnImagem1.Location = new Point(161, 397);
             btnImagem1.Name = "btnImagem1";
-            btnImagem1.Size = new Size(170, 32);
+            btnImagem1.Size = new Size(187, 32);
             btnImagem1.TabIndex = 1;
             btnImagem1.Text = "Carregar imagem 1";
             btnImagem1.UseVisualStyleBackColor = true;
@@ -65,9 +71,9 @@
             // 
             imgCarregada.Anchor = AnchorStyles.None;
             imgCarregada.BackColor = Color.FromArgb(30, 30, 30);
-            imgCarregada.Location = new Point(35, 124);
+            imgCarregada.Location = new Point(85, 32);
             imgCarregada.Name = "imgCarregada";
-            imgCarregada.Size = new Size(255, 255);
+            imgCarregada.Size = new Size(350, 350);
             imgCarregada.SizeMode = PictureBoxSizeMode.StretchImage;
             imgCarregada.TabIndex = 2;
             imgCarregada.TabStop = false;
@@ -76,9 +82,9 @@
             // 
             imgFinal.Anchor = AnchorStyles.None;
             imgFinal.BackColor = Color.FromArgb(30, 30, 30);
-            imgFinal.Location = new Point(792, 124);
+            imgFinal.Location = new Point(1316, 32);
             imgFinal.Name = "imgFinal";
-            imgFinal.Size = new Size(255, 255);
+            imgFinal.Size = new Size(350, 350);
             imgFinal.SizeMode = PictureBoxSizeMode.StretchImage;
             imgFinal.TabIndex = 3;
             imgFinal.TabStop = false;
@@ -86,8 +92,8 @@
             // btnCinza
             // 
             btnCinza.Anchor = AnchorStyles.None;
-            btnCinza.Font = new Font("JetBrains Mono", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnCinza.Location = new Point(455, 124);
+            btnCinza.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCinza.Location = new Point(866, 127);
             btnCinza.Name = "btnCinza";
             btnCinza.Size = new Size(170, 32);
             btnCinza.TabIndex = 4;
@@ -95,18 +101,95 @@
             btnCinza.UseVisualStyleBackColor = true;
             btnCinza.Click += btnCinza_Click;
             // 
+            // btnBrilho
+            // 
+            btnBrilho.Anchor = AnchorStyles.None;
+            btnBrilho.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnBrilho.Location = new Point(866, 184);
+            btnBrilho.Name = "btnBrilho";
+            btnBrilho.Size = new Size(170, 32);
+            btnBrilho.TabIndex = 5;
+            btnBrilho.Text = "Aumentar brilho";
+            btnBrilho.UseVisualStyleBackColor = true;
+            btnBrilho.Click += btnBrilho_Click;
+            // 
+            // valorBrilho
+            // 
+            valorBrilho.Anchor = AnchorStyles.None;
+            valorBrilho.BorderStyle = BorderStyle.None;
+            valorBrilho.Font = new Font("Arial", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            valorBrilho.Location = new Point(1042, 184);
+            valorBrilho.MinimumSize = new Size(180, 30);
+            valorBrilho.Name = "valorBrilho";
+            valorBrilho.PlaceholderText = "Valor adicionado";
+            valorBrilho.Size = new Size(180, 30);
+            valorBrilho.TabIndex = 6;
+            valorBrilho.TextAlign = HorizontalAlignment.Center;
+            // 
+            // btnNegativo
+            // 
+            btnNegativo.Anchor = AnchorStyles.None;
+            btnNegativo.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnNegativo.Location = new Point(866, 237);
+            btnNegativo.Name = "btnNegativo";
+            btnNegativo.Size = new Size(170, 32);
+            btnNegativo.TabIndex = 7;
+            btnNegativo.Text = "Imagem negativa";
+            btnNegativo.UseVisualStyleBackColor = true;
+            btnNegativo.Click += btnNegativo_Click;
+            // 
+            // btnContraste
+            // 
+            btnContraste.Anchor = AnchorStyles.None;
+            btnContraste.Font = new Font("Microsoft Sans Serif", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnContraste.Location = new Point(866, 291);
+            btnContraste.Name = "btnContraste";
+            btnContraste.Size = new Size(170, 32);
+            btnContraste.TabIndex = 8;
+            btnContraste.Text = "Contraste";
+            btnContraste.UseVisualStyleBackColor = true;
+            // 
+            // imgCarregada2
+            // 
+            imgCarregada2.Anchor = AnchorStyles.None;
+            imgCarregada2.BackColor = Color.FromArgb(30, 30, 30);
+            imgCarregada2.Location = new Point(454, 32);
+            imgCarregada2.Name = "imgCarregada2";
+            imgCarregada2.Size = new Size(350, 350);
+            imgCarregada2.SizeMode = PictureBoxSizeMode.StretchImage;
+            imgCarregada2.TabIndex = 10;
+            imgCarregada2.TabStop = false;
+            // 
+            // btnImagem2
+            // 
+            btnImagem2.Anchor = AnchorStyles.None;
+            btnImagem2.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnImagem2.Location = new Point(530, 397);
+            btnImagem2.Name = "btnImagem2";
+            btnImagem2.Size = new Size(187, 32);
+            btnImagem2.TabIndex = 9;
+            btnImagem2.Text = "Carregar imagem 2";
+            btnImagem2.UseVisualStyleBackColor = true;
+            btnImagem2.Click += btnImagem2_Click;
+            // 
             // Home
             // 
-            AutoScaleDimensions = new SizeF(7F, 16F);
+            AutoScaleDimensions = new SizeF(10F, 18F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(20, 20, 20);
-            ClientSize = new Size(1084, 603);
+            ClientSize = new Size(1761, 624);
+            Controls.Add(imgCarregada2);
+            Controls.Add(btnImagem2);
+            Controls.Add(btnContraste);
+            Controls.Add(btnNegativo);
+            Controls.Add(valorBrilho);
+            Controls.Add(btnBrilho);
             Controls.Add(btnCinza);
             Controls.Add(imgFinal);
             Controls.Add(imgCarregada);
             Controls.Add(btnImagem1);
             Controls.Add(titulo);
-            Font = new Font("JetBrains Mono", 8.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Font = new Font("Microsoft Sans Serif", 8.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
             Name = "Home";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Alterador de imagens";
@@ -114,7 +197,9 @@
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)imgCarregada).EndInit();
             ((System.ComponentModel.ISupportInitialize)imgFinal).EndInit();
+            ((System.ComponentModel.ISupportInitialize)imgCarregada2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -124,5 +209,11 @@
         private PictureBox imgCarregada;
         private PictureBox imgFinal;
         private Button btnCinza;
+        private Button btnBrilho;
+        private TextBox valorBrilho;
+        private Button btnNegativo;
+        private Button btnContraste;
+        private PictureBox imgCarregada2;
+        private Button btnImagem2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,16 @@
 namespace AlteradorDeImagens
 {
-    public partial class Home : Form
-    {
+    public partial class Home : Form {
         String imageURL = "";
-        public Home()
-        {
+        String imageURL2 = "";
+        public Home() {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+        private void Form1_Load(object sender, EventArgs e) {
 
         }
-
-        private void btnImagem1_Click(object sender, EventArgs e)
-        {
+        private void btnImagem1_Click(object sender, EventArgs e) {
             try
             {
                 OpenFileDialog dialog = new OpenFileDialog();
@@ -33,18 +29,35 @@ namespace AlteradorDeImagens
 
         }
 
-        private void btnCinza_Click(object sender, EventArgs e)
-        {
-            Bitmap bmt = new Bitmap(imageURL);
-            int width = bmt.Width;
-            int height = bmt.Height;
+        private void btnImagem2_Click(object sender, EventArgs e) {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "jpg files(*.jpg)|*.jpg| PNG files(*.png)|*.png";
+
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    imageURL2 = dialog.FileName;
+                    imgCarregada2.ImageLocation = imageURL2;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("O tipo da imagem esta errado, ou algum erro ocorreu", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCinza_Click(object sender, EventArgs e) {
+            Bitmap bmp = new Bitmap(imageURL);
+            int width = bmp.Width;
+            int height = bmp.Height;
             Color p;
 
-            for(int y = 0; y < height; y++) 
+            for (int y = 0; y < height; y++)
             {
-                for(int x = 0; x < width; x++)
+                for (int x = 0; x < width; x++)
                 {
-                    p = bmt.GetPixel(x, y);
+                    p = bmp.GetPixel(x, y);
 
                     int a = p.A;
                     int r = p.R;
@@ -52,10 +65,65 @@ namespace AlteradorDeImagens
                     int b = p.B;
 
                     int avg = (r + g + b) / 3;
-                    bmt.SetPixel(x, y, Color.FromArgb(a, avg, avg, avg));
+                    bmp.SetPixel(x, y, Color.FromArgb(a, avg, avg, avg));
                 }
             }
-            imgFinal.Image = bmt;
+            imgFinal.Image = bmp;
+        }
+
+        private void btnBrilho_Click(object sender, EventArgs e) {
+            Bitmap bmp = new Bitmap(imageURL);
+            int width = bmp.Width;
+            int height = bmp.Height;
+            Color p;
+
+            int valorSomar = Int32.Parse(valorBrilho.Text);
+
+            if (valorSomar > 255) valorSomar = 255;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    p = bmp.GetPixel(x, y);
+
+                    int a = p.A + valorSomar;
+                    int r = p.R + valorSomar;
+                    int g = p.G + valorSomar;
+                    int b = p.B + valorSomar;
+
+                    if (a > 255) a = 255;
+                    if (r > 255) r = 255;
+                    if (g > 255) g = 255;
+                    if (b > 255) b = 255;
+
+                    bmp.SetPixel(x, y, Color.FromArgb(a, red: r, green: g, blue: b));
+                }
+            }
+            imgFinal.Image = bmp;
+        }
+
+        private void btnNegativo_Click(object sender, EventArgs e) {
+            Bitmap bmp = new Bitmap(imageURL);
+            int width = bmp.Width;
+            int height = bmp.Height;
+            Color p;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    p = bmp.GetPixel(x, y);
+
+                    int a = 255 - p.A;
+                    int r = 255 - p.R;
+                    int g = 255 - p.G;
+                    int b = 255 - p.B;
+
+                    bmp.SetPixel(x, y, Color.FromArgb(a, red: r, green: g, blue: b));
+                }
+            }
+            imgFinal.Image = bmp;
         }
     }
 }
